9f2bb0b7ff20554b6185001dcd7a5a167b167599
Reymon <rvega@jaumebalmes.net>
2022-11-10 20:51:45 +0100

    Segundo Ejercicio

---
+++ invest.py
@@ -0,0 +1,11 @@
+#Creando Funcion
+def calculo():
+    #Variable
+    valor = 100
+    #Añadiendo for para recorrer
+    for i in range (7):
+        valor *= 1.1
+    #Imprimiendo
+    print(valor)
+#Llamando a la funcion
+calculo()
